9dc1c3b00df762a876687484545619bcd1340718
S00fTik <kurevins@mail.ru>
2022-05-30 16:27:52 +0300

    Calc

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Calck
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            double a;
+            double b;
+            double res = 0;
+            char oper;
+
+            Console.Write("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("\nВведите оператор:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("\nВведите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (oper == '+')
+            {
+                res = a + b;
+            }
+
+            else if (oper == '-')
+            {
+                res = a - b;
+            }
+
+            else if (oper == '*')
+            {
+                res = a * b;
+            }
+
+            else if (oper == '/')
+            {
+
+                res = a / b;
+            }
+            Console.WriteLine("\nРезультат: {0}", res);
+            Console.ReadLine();
+        }
+    }
+}
